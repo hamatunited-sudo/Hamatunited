7925da0caee8b7e50a8410b38b85f2e71a6f1213
hamatunited-sudo <hamatunited@gmail.com>
2025-10-06 16:59:48 +0300

    Add missing content.json and useContent hook to fix module not found error

---
+++ src/hooks/useContent.ts
@@ -0,0 +1,13 @@
+export interface ContentData {
+  ar: {
+    [key: string]: any;
+  };
+  en: {
+    [key: string]: any;
+  };
+}
+
+export const useContent = () => {
+  // This hook can be expanded later for content management
+  return {};
+};
